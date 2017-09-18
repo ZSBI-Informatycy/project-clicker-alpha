036c686e08068316cd2c336f7bdffb2d54d29eb6
ZSBI <claymorealt@op.pl>
2017-09-18 18:35:15 +0200

    Added jQuery, basic canvas operation testing and linked jQuery to index.html.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-var canvas = document.getElementById('bigClicker');
-var context = canvas.getContext('2d');
-
-context.fillStyle = '#F00';
-context.fillRect(0, 0, canvas.width, canvas.height);
+$(document).ready(function () {
+    var canvas = document.getElementById('bigClicker');
+    var context = canvas.getContext('2d');
+    
+    context.fillStyle = '#F00';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+});
