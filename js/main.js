75ea4a1489d8350a0f2cd47a46ee51abd54671d7
ZSBI <claymorealt@op.pl>
2017-09-19 21:32:16 +0200

    Added basic clicking mechanism and economics. No clicking yet though.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
-$(document).ready(function () {
+// Global values
+
+// Global amount of cookies earned.
+var cookieCount = 0;
+
+// For the big "clicker" object
+function initClicker() {
     var canvas = document.getElementById('bigClicker');
     var context = canvas.getContext('2d');
     
     context.fillStyle = '#F00';
     context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// Clicking Maths
+function earn(amount) {
+    cookieCount += amount;
+}
+
+// Clicking the clicker;
+function clickEvent(event, amount) {
+    var computedMouseCps = 1;
+    var lastClick = 0;
+    var canClick = 1;
+    
+    var now = Date.now();
+    if(event) event.preventDefault();
+    else if(now-lastClick < 1000/250) {}
+    else {
+        var amount = amount ? amount : computedMouseCps;
+        earn(amount);
+        cookieCount += amount;
+    }
+    lastClick = now;
+}
+
+// Main document load module
+$(document).ready(function () {
+    initClicker();
 });
