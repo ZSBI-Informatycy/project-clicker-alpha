70704bd9b5c0a543448ce392cd956b0673bfd34c
ZSBI <claymorealt@op.pl>
2017-09-20 17:21:20 +0200

    Structured as an object. Global values now in the local scope. Cleaner code and more maths

--- js/main.js
+++ js/main.js
@@ -1,38 +1,60 @@
-// Global values
+// Utility functions
 
-// Global amount of cookies earned.
-var cookieCount = 0;
+// Beautify the numbers
+function Beautify(what, floats) {
+    var str = '';
+    what =  Math.round(what*100000)/100000; // Rounding error prevention
+    if(floats>0) {
+        var floater = what-Math.floor(what);
+        floater = Math.round(floater*100000)/100000;
+    }
+}
 
-// For the big "clicker" object
-function initClicker() {
-    var canvas = document.getElementById('bigClicker');
-    var context = canvas.getContext('2d');
+
+Game = {};
+
+Game.launch = function() {
+    // Global click amount
+    Game.clickCount = 0;
     
-    context.fillStyle = '#F00';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-// Clicking Maths
-function earn(amount) {
-    cookieCount += amount;
-}
-
-// Clicking the clicker;
-function clickEvent(event, amount) {
-    var computedMouseCps = 1;
-    var lastClick = 0;
-    var canClick = 1;
+    Game.initClicker = function() {
+        var canvas = document.getElementById('bigClicker');
+        var context = canvas.getContext('2d');
     
-    var now = Date.now();
-    if(now-lastClick < 1000/250) {}
-    var amount = amount ? amount : computedMouseCps;
-    earn(amount);
-    cookieCount += amount;
-    lastClick = now;
-    console.log(cookieCount);
+        context.fillStyle = '#722210';
+        context.beginPath();
+        context.arc(canvas.width/2, canvas.height/2, 125, 0, 2*Math.PI);
+        context.fill();
+    };
+
+    // Clicking Maths
+    Game.earn = function(amount) {
+        Game.clickCount += amount;
+    };
+
+    // Clicking the clicker;
+    Game.clickEvent = function(event, amount) {
+        var computedMouseCps = 1;
+        var lastClick = 0;
+        var canClick = 1;
+        var now = Date.now();
+        if(now-lastClick < 1000/250) {}
+        var amount = amount ? amount : computedMouseCps;
+        Game.earn(amount);
+        Game.clickCount += amount;
+        console.log(clickCount);
+        lastClick = now;
+    };
+    
+    // Click display
+    Game.clickDisplay = function(amount) {
+        document.getElementById('clickCounter').innerHTML = Game.clickCount;
+    }
+
+    // Main document load module
+    Game.initClicker();
 }
 
-// Main document load module
 $(document).ready(function () {
-    initClicker();
+    Game.launch();
 });
